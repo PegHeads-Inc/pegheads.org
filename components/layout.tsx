f0f4c76137171b962eea165739aabbbe6b272239
Devin <devincapriola@gmail.com>
2023-01-15 16:53:56 -0500

    added layout

---
+++ components/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "@components/header";
+import Footer from "@components/footer";
+
+export default function Layout({ children }: any) {
+  return (
+    <>
+    <Header />
+      <div className="text-gray-600 dark:text-gray-400">
+        {children}
+      </div>
+    <Footer />
+    </>
+  );
+}
