bbf493d259bdc0e9bbbc5a5b0d51459cfda90f09
Devin <devincapriola@gmail.com>
2023-01-15 16:59:16 -0500

    updated _app.tsx and file paths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+
+import Layout from '@/components/layout';
+import SEO from '@/components/SEO';
+
+console.log(`%c
+██████╗░███████╗░██████╗░██╗░░██╗███████╗░█████╗░██████╗░░██████╗░░░░█████╗░██████╗░░██████╗░
+██╔══██╗██╔════╝██╔════╝░██║░░██║██╔════╝██╔══██╗██╔══██╗██╔════╝░░░██╔══██╗██╔══██╗██╔════╝░
+██████╔╝█████╗░░██║░░██╗░███████║█████╗░░███████║██║░░██║╚█████╗░░░░██║░░██║██████╔╝██║░░██╗░
+██╔═══╝░██╔══╝░░██║░░╚██╗██╔══██║██╔══╝░░██╔══██║██║░░██║░╚═══██╗░░░██║░░██║██╔══██╗██║░░╚██╗
+██║░░░░░███████╗╚██████╔╝██║░░██║███████╗██║░░██║██████╔╝██████╔╝██╗╚█████╔╝██║░░██║╚██████╔╝
+╚═╝░░░░░╚══════╝░╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═════╝░╚═════╝░╚═╝░╚════╝░╚═╝░░╚═╝░╚═════╝░
+`, 'color: #fff; font-size: 15px; font-family: monospace;');
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <SEO />
+    <ThemeProvider attribute="class">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+    </>
+  );
 }
